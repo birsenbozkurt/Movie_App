bb26001cb491815d52c342dca65ebdb411e7d9c1
birsenbozkurt <birsenbozkurt4041@gmail.com>
2023-01-21 20:31:23 +0300

    nav-bar categories

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,7 @@ import Card from "./Card";
 let API_key = "&api_key=6271e7f0da98b9d66afcd07c890d72b6";
 let base_url = "https://api.themoviedb.org/3";
 let url = base_url + "/discover/movie?sort_by=popularity.desc" + API_key;
+let arr = ["Popular", "Theatre", "Kids", "Drama", "Comedie"];
 const Main = () => {
 	const [moviedata, setData] = useState([]);
 	const [url_set, setUrl] = useState(url);
@@ -18,26 +19,44 @@ const Main = () => {
 				setData(data.results);
 			});
 	}, [url_set]);
+	const getData = (movieType) => {
+		if (movieType == "Popular") {
+			url = base_url + "/discover/movie?sort_by=popularity.desc" + API_key;
+		}
+		if (movieType == "Theatre") {
+			url = base_url + "/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22" + API_key;
+		}
+		if (movieType == "Kids") {
+			url = base_url + "/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc" + API_key;
+		}
+		if (movieType == "Drama") {
+			url = base_url + "/discover/movie?with_genres=18&primary_release_year=2014" + API_key;
+		}
+		if (movieType == "Comedie") {
+			url = base_url + "/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc" + API_key;
+		}
+		setUrl(url);
+	};
 	return (
 		<>
 			<div className="header">
 				<nav>
 					<ul>
-						<li>
-							<a href="#">Popular</a>
-						</li>
-						<li>
-							<a href="#">Theatre</a>
-						</li>
-						<li>
-							<a href="#">Kids</a>
-						</li>
-						<li>
-							<a href="#">Drama</a>
-						</li>
-						<li>
-							<a href="#">Comedie</a>
-						</li>
+						{arr.map((value) => {
+							return (
+								<li>
+									<a
+										href="#"
+										name={value}
+										onClick={(e) => {
+											getData(e.target.name);
+										}}
+									>
+										{value}
+									</a>
+								</li>
+							);
+						})}
 					</ul>
 				</nav>
 				<form>
