c0a39dbab7da7441c5ddd8c7b55ea5ddb57187e3
birsenbozkurt <birsenbozkurt4041@gmail.com>
2023-01-21 18:51:10 +0300

    images and movies

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,18 +2,19 @@ import react from "react";
 
 const Card = (movie) => {
 	console.log(movie.info);
+	let img_path = "https://image.tmdb.org/t/p/w500";
 	return (
 		<>
 			<div className="movie">
-				<img src="images/poster.jpg" className="poster" alt="" />
+				<img src={img_path + movie.info.poster_path} className="poster" alt="" />
 				<div className="movie-details">
 					<div className="box">
-						<h4 className="title">Movie Title</h4>
-						<p className="rating">9.7</p>
+						<h4 className="title">{movie.info.title}</h4>
+						<p className="rating">{movie.info.vote_average}</p>
 					</div>
 					<div className="overview">
 						<h1>overview</h1>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, suscipit ab. Quisquam, reprehenderit sequi velit
+						{movie.info.overview}
 					</div>
 				</div>
 			</div>
